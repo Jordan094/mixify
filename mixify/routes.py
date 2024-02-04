bd6dee9072ee8f93d2cdb23a957bdd41643f1ef5
Jordan Cooper <jordan_cooper@outlook.com>
2024-02-04 12:56:39 +0000

    Added favourites page and linked in navbar

--- mixify/routes.py
+++ mixify/routes.py
@@ -4,3 +4,7 @@ from mixify import app, db
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/favourites")
+def favourites():
+    return render_template("favourites.html")
