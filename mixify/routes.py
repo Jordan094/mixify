93b420ed3a2067b060327230dbf5fd0dfe1d73b1
Jordan Cooper <jordan_cooper@outlook.com>
2024-02-04 10:20:25 +0000

    Initial Commit

---
+++ mixify/routes.py
@@ -0,0 +1,6 @@
+from flask import render_template
+from mixify import app, db
+
+@app.route("/")
+def home():
+    return render_template("base.html")
---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from mixify import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=os.environ.get("PORT"),
+        debug=os.environ.get("DEBUG")
+    )
