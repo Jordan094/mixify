d381037490aa6391acff084039bf9fe8a43913d6
Jordan Cooper <jordan_cooper@outlook.com>
2024-02-07 18:58:21 +0000

    Created model for recipe

--- mixify/models.py
+++ mixify/models.py
@@ -7,3 +7,12 @@ class User(db.Model):
     user_first_name = db.Column(db.String(50), nullable=False)
     user_last_name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(260), nullable=False)
+
+class Recipe(db.Model):
+    # Schema for the Recipe model
+    recepie-id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    image_path = db.Column(db.String(100), nullable=True)
