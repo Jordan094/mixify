070e0f722c9e1837c402be49ccf2d0eb497527ae
Jordan Cooper <jordan_cooper@outlook.com>
2024-02-10 21:51:06 +0000

    Updated Navbar

--- mixify/routes.py
+++ mixify/routes.py
@@ -98,7 +98,7 @@ def delete_recipe(recipe_id):
     # Check if the current user is the submitter of the recipe
     if current_user != recipe.submitter_username:
         flash("You are not authorized to delete this recipe.")
-        return redirect(url_for("home"))
+        return redirect(url_for("recipes"))
 
     # Delete the recipe from the database
     db.session.delete(recipe)
