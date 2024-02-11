b19d5308954abf09f67cd7d2dc902795fd8862a5
Jordan Cooper <jordan_cooper@outlook.com>
2024-02-11 09:02:46 +0000

    Updated code comments on models.py for code readbility

--- mixify/models.py
+++ mixify/models.py
@@ -1,16 +1,18 @@
-from mixify import db
+from mixify import db # Import the db instance from the mixify package
 
+# Define the User model
 class User(db.Model):
-    # schema for the User model
-    id = db.Column(db.Integer, primary_key=True)
+    # Define the columns for the User table
+    id = db.Column(db.Integer, primary_key=True) # Primary key column for user ID
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     user_first_name = db.Column(db.String(50), nullable=False)
     user_last_name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(260), nullable=False)
 
+# Define the Recipe model
 class Recipe(db.Model):
-    # Schema for the Recipe model
-    recepieid = db.Column(db.Integer, primary_key=True)
+    # Define the columns for the Recipe table
+    recepieid = db.Column(db.Integer, primary_key=True) # Primary key column for recipe ID
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
